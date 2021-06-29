cc90f7f12775ac7acda9c9bebda895501db852fa
MRGRD56 <kirgol85@gmail.com>
2021-06-29 15:08:14 +0500

    Add project files.

---
+++ HCTabControlApp/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCTabControlApp
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Tab> Tabs1 { get; } = new();
+        public ObservableCollection<Tab> Tabs2 { get; } = new();
+
+        public MainWindowViewModel()
+        {
+            Tabs1.Add(new Tab("One"));
+            Tabs1.Add(new Tab("Two"));
+            Tabs1.Add(new Tab("Three"));
+
+            Tabs2.Add(new Tab("One"));
+            Tabs2.Add(new Tab("Two"));
+            Tabs2.Add(new Tab("Three"));
+        }
+    }
+}
---
+++ HCTabControlApp/Tab.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HCTabControlApp
+{
+    public class Tab : NotifyPropertyChanged
+    {
+        public Tab(string header)
+        {
+            Header = header;
+            NewHeader = header;
+        }
+
+        private string _header;
+
+        public string Header
+        {
+            get => _header;
+            set
+            {
+                if (value == _header) return;
+                _header = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string NewHeader { get; set; }
+        
+        public ICommand UpdateHeadingCommand => new Command<object>(() =>
+        {
+            Header = NewHeader;
+        });
+    }
+}
